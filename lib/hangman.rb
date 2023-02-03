18dd96769febb13fd84e0814d014c0ad9c4e9d54
Mapt04 <35663475+Mapt04@users.noreply.github.com>
2023-02-02 23:34:37 -0300

    Add save and load game functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,13 +1,18 @@
 require_relative 'hangman_images.rb'
+require 'securerandom'
 
 dictionary = File.read('dictionary.txt')
 dictionary_array = dictionary.split("\n")
 
 class Game
 
-    def initialize(dictionary)
+    def initialize(dictionary, save_file=nil)
         @solution = dictionary.sample
         @guessed = []
+        if save_file
+           @save_file = save_file
+           load
+        end 
     end
 
     def play
@@ -26,12 +31,8 @@ class Game
     end
 
     def display_status
-        status = @solution.split("").join(" ")
-        not_guessed.each do |letter|
-            status.gsub!(letter, "_")
-        end
+        
         puts status
-
         puts HANGMANIMAGES[wrong_guesses]
         puts "Wrong guesses: #{wrong_guesses}"
         puts "Letters guessed: #{@guessed.join(', ')}"
@@ -39,15 +40,47 @@ class Game
         puts
     end
 
+    def status
+        status_string = @solution.split("").join(" ")
+        not_guessed.each do |letter|
+            status_string.gsub!(letter, "_")
+        end
+        status_string
+    end
+
     def pick_letter
         loop do
-            print "Guess a letter: "
+            print "Guess a letter ('save' to save game): "
             guess = gets.chomp.downcase
             if not_guessed.include? guess
                 return guess
             end
+
+            if guess == 'save'
+                save
+                next
+            end
+            
             puts "Invalid guess."
         end
+    end
+
+    def save
+        Dir.mkdir('saves') unless Dir.exist?('saves')
+
+        @save_file = "saves/#{SecureRandom.hex}.txt" unless @save_file
+
+        File.open(@save_file, 'w') do |file|
+            file.puts @solution
+            file.print @guessed.join(",")
+        end
+        puts "Game state saved."
+    end
+
+    def load()
+        save_info = File.read(@save_file).split("\n")
+        @solution = save_info[0]
+        @guessed = save_info[1].split(",")
     end
 
     def not_guessed
@@ -67,8 +100,48 @@ class Game
     end
 end
 
+def choose_load_option
+    loop do
+        puts "(1) New game"
+        puts "(2) Load game"
+        print "Enter: "
+        selection = gets.to_i
+        return selection if selection in 1..2
+        puts "Invalid choice.\n"
+    end
+end
+
+def saves
+    saves = Dir["saves/*.txt"]
+end
+
+def show_save_files
+    saves.each_with_index do |save_file, index|
+        status = Game.new([], save_file).status
+        puts ("(#{index+1})  #{status}")
+    end
+end
+
+def choose_save_file(saves)
+    loop do
+        puts
+        show_save_files
+        print "Select a file: "
+        selection = gets.to_i
+        return saves[selection-1] if (1..saves.length).include?(selection)
+        puts "Invalid choice.\n"
+    end
+end
+
 puts "===== Welcome to Hangman! =====\n\n"
 
-game = Game.new(dictionary_array)
+load_option = choose_load_option
 
+puts
+
+if load_option == 2
+    save_file = choose_save_file(saves)
+end
+
+game = Game.new(dictionary_array, save_file)
 game.play
